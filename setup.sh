0856f802a6690157b987bc785641324e62e8ea97
alecfong1 <alecfong1@gmail.com>
2023-03-24 22:44:10 +0000

    fixing pull

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ mkdir ~/stable-diffusion/checkpoints
 conda init bash
 conda init zsh
 conda env create -f ~/stable-diffusion/environment.yaml
-wget https://brev-bucket-for-model-storage.s3.us-west-2.amazonaws.com/sd-v1-4.ckpt > ~/stable-diffusion/checkpoints/sd-v1-4.ckpt
+wget https://brev-bucket-for-model-storage.s3.us-west-2.amazonaws.com/sd-v1-4.ckpt -P ~/stable-diffusion/checkpoints
